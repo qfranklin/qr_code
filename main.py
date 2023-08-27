eea5c7a42346dc1f8bc1b2b558508824c2c3b7ce
Quentin Franklin <qfranklin906@gmail.com>
2023-08-27 11:52:13 -0400

    moved util functions into their own file, moved config variables into thier own file

--- main.py
+++ main.py
@@ -1,102 +1,21 @@
 import bpy
 import mathutils
 import datetime
-import bmesh
-import numpy as np
-from scipy.spatial import ConvexHull
-import math
 import os
-import time
+import sys
+import importlib
 
-SCALE = 1
-SCENE_SYSTEM = 'METRIC'
-SCENE_UNITS = 'MILLIMETERS'
+# TODO: get this sensitive data into a secure file
+CURRENT_DIR = "C:\\Users\\qfran\\Desktop\\Blender\\code\\qr_code\\"
 
-QR_CODE_SIZE = 20
-QR_SPACING = 0
-QR_CODE_THICKNESS = .2
-QUIET_ZONE = 8
+# Add the path to your scripts to sys.path
+if CURRENT_DIR not in sys.path:
+    sys.path.append(CURRENT_DIR)
 
-BASEPLATE_THICKNESS = .2
 
-TEXT_FILE_LOCATION = 'C:\\Users\\qfran\\Desktop\\Blender\\code\\qr_code\\blockbit.ttf'
-
-GRID_SIZE = 2
-
-# Have to adjust these based on text. A real sicko would figure out how to automate this. 
-FONT_SIZE = 6
-TEXT_X_OFFSET = 4
-TEXT_Y_OFFSET = -1
-
-# If the bottom layer is transparent, then invert the code so you can put a sticker as the back
-INVERT_QR_CODE = True
-
-def start():
-    # Clear the console
-    from os import system; 
-    cls = lambda: system('cls'); 
-    cls()
-
-    # Get the current date and time
-    current_time = datetime.datetime.now()
-
-    print("\n======================================================")
-    print("Current Date and Time:", current_time)
-    print("======================================================")
-
-    dimension_check()
-    clear_scene()
-
-    return current_time
-
-def end(current_time):
-    bpy.ops.export_mesh.stl(filepath="C:\\Users\\qfran\\Desktop\\Blender\\code\\qr_code\\output\\file.stl")
-
-    # Calculate the elapsed time in milliseconds
-    elapsed_time = round((datetime.datetime.now() - current_time).total_seconds(), 2)
-    print("\n======================================================")
-    print("Run Time:", elapsed_time, "seconds")
-    print("======================================================\n")
-
-    #print(f"Memory used during script execution: {memory_used} bytes")
-    
-def dimension_check():
-    scene = bpy.context.scene
-    print("Unit Scale:", setattr(scene.unit_settings, 'scale_length', SCALE) or scene.unit_settings.scale_length)
-    print("Length Unit:", setattr(scene.unit_settings, 'length_unit', SCENE_UNITS) or scene.unit_settings.length_unit)
-    print("Unit System:", setattr(scene.unit_settings, 'system', SCENE_SYSTEM) or scene.unit_settings.system, "\n")
-
-def clear_scene():
-    if bpy.context.object and bpy.context.object.mode != 'OBJECT':
-        bpy.ops.object.mode_set(mode='OBJECT')
-        
-    # Delete objects
-    objects_to_delete = list(bpy.data.objects)
-    for obj in objects_to_delete:
-        #print('Deleting object:',obj.name)
-        bpy.data.objects.remove(obj)
-
-    # Delete collections
-    for collection in bpy.data.collections:
-        #print('Deleting collection:',collection.name)
-        bpy.data.collections.remove(collection)
-
-def merge_objects(collection):
-    # Convert all objects in the collection to meshes
-    bpy.ops.object.select_all(action='DESELECT')
-    for obj in collection.objects:
-        obj.select_set(True)
-        bpy.context.view_layer.objects.active = obj
-        bpy.ops.object.convert(target='MESH')
-
-    # Join all converted mesh objects
-    bpy.ops.object.select_all(action='DESELECT')
-    for obj in collection.objects:
-        obj.select_set(True)
-    bpy.context.view_layer.objects.active = collection.objects[0]
-    bpy.ops.object.join()
-
-    return bpy.context.view_layer.objects.active
+from config import *
+from utils import *
+  
 
 def import_image(svg_file_path):
 
@@ -143,14 +62,7 @@ def import_image(svg_file_path):
 
     # Return the joined curve object
     return bpy.context.view_layer.objects.active
-
-def debug_object(obj):
-    print(f"    Name: {obj.name}")
-    for collection in obj.users_collection:
-        print(f"    In collection: {collection.name}")
-    print(f"    Scale Factor (x,y,z): ({round(obj.scale.x, 2)}, {round(obj.scale.y, 2)}, {round(obj.scale.z, 2)})")
-    print(f"    Dimensions (x,y,z): ({', '.join([f'{dimension:.2f}' for dimension in obj.dimensions])}) {SCENE_UNITS}")
-        
+     
 def add_timestamp(text, qr_code_obj):
 
     bpy.ops.object.text_add(enter_editmode=True, align='WORLD', location=(0, 0, 0))
@@ -246,19 +158,10 @@ def center_object(obj):
 
     return bpy.context.view_layer.objects.active
 
-def solidify_object(invert_code):
-    bpy.ops.object.modifier_add(type='SOLIDIFY')
-    bpy.context.object.modifiers["Solidify"].thickness = QR_CODE_THICKNESS 
-
-    if invert_code:
-        bpy.context.object.modifiers["Solidify"].offset = 1
-    
-    bpy.ops.object.modifier_apply(modifier="Solidify")
-
 def main():
     current_time = start()
     
-    input_dir = "C:\\Users\\qfran\\Desktop\\Blender\\code\\qr_code\\input\\"
+    input_dir = CURRENT_DIR + "input\\"
 
     # Filter out only the SVG files
     svg_files = [f for f in os.listdir(input_dir) if f.endswith(".svg")]
@@ -270,36 +173,11 @@ def main():
     # List to store all the QR code objects
     qr_objects = []
 
-    date = datetime.date.today()
-
-    current_date = date.strftime('%Y-%m-%d')
+    # date = datetime.date.today()
+    # current_date = date.strftime('%Y-%m-%d')
 
     input_names = [
-        "SL-1",
-        "SL-1",
-        "SL-1",
-        "SL-1",
-        "SL-1",
-        "SL-1",
-        "SL-1",
-        "SL-1",
-        "SL-1",
-        "SL-1",
-        "SL-1",
-        "SL-1",
-        "SL-1",
-        "SL-1",
-        "SL-1",
-        "SL-1",
-        "SL-1",
-        "SL-1",
-        "SL-1",
-        "SL-1",
-        "SL-1",
-        "SL-1",
-        "SL-1",
-        "SL-1",
-        "SL-1"
+        "Github"
     ];
 
     for index, filename in enumerate(svg_files):
