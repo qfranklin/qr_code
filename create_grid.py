ee7487d294e7c79b3a431dec6146835196faf6ef
Quentin Franklin <qfranklin906@gmail.com>
2023-09-03 13:48:46 -0400

    changed to 3 by 3 grid, removed unused parameter

--- create_grid.py
+++ create_grid.py
@@ -1,7 +1,7 @@
 from gimpfu import *
 import textwrap
 
-def draw_grid(image, layer, x1, y1, x2, y2):
+def draw_grid(layer, x1, y1, x2, y2):
     pdb.gimp_context_set_foreground((0, 0, 0))  # Set color to black
     pdb.gimp_paintbrush_default(layer, 4, [x1, y1, x2, y1])
     pdb.gimp_paintbrush_default(layer, 4, [x1, y2, x2, y2])
@@ -35,43 +35,44 @@ def place_text(image, drawable, x, y, text):
 
 def create_image():
 
-    # Assuming 10 pixels = 1mm
-    width_pixels = 86 * 10
-    height_pixels = 103 * 10
-
-    # The total size of the image will be 86x103 mm
-    total_size_width = width_pixels
-    total_size_height = height_pixels
-
-    # Gap between grid cells, which is half for a 2x2 grid
-    gap_width = width_pixels / 2
-    gap_height = height_pixels / 2
+    gap_in_pixels = 500  # Pixels (10 pixels = 1mm)
+    total_size = gap_in_pixels * 3
 
     # Create a new RGB image
-    image = gimp.Image(total_size_width, total_size_height, RGB)
-    layer = gimp.Layer(image, "Background", total_size_width, total_size_height, RGB_IMAGE, 100, NORMAL_MODE)
+    image = gimp.Image(total_size, total_size, RGB)
+    layer = gimp.Layer(image, "Background", total_size, total_size, RGB_IMAGE, 100, NORMAL_MODE)
     image.add_layer(layer, 0)
-    gimp.set_background((255, 255, 255))
+    gimp.set_background((255, 255, 255))  # Set background color to white
     drawable = pdb.gimp_image_get_active_layer(image)
     pdb.gimp_drawable_fill(drawable, BACKGROUND_FILL)
 
-    # Draw vertical grid line
-    draw_grid(image, drawable, gap_width, 0, gap_width, total_size_height)
-    # Draw horizontal grid line
-    draw_grid(image, drawable, 0, gap_height, total_size_width, gap_height)
+    draw_grid(drawable, gap_in_pixels, 0, gap_in_pixels, total_size)
+    draw_grid(drawable, total_size - gap_in_pixels, 0, total_size - gap_in_pixels, total_size)
+    draw_grid(drawable, 0, gap_in_pixels, total_size, gap_in_pixels)
+    draw_grid(drawable, 0, total_size - gap_in_pixels, total_size, total_size - gap_in_pixels)
 
-    # Centers for 2x2 grid
+    # Display the image
+    gimp.Display(image)
+    gimp.displays_flush()
+
+     # Place the text at the center of each grid block
     centers = [
-        (gap_width / 2, gap_height / 2),       # Top-left square
-        (gap_width * 1.5, gap_height / 2),     # Top-right square
-        (gap_width / 2, gap_height * 1.5),     # Bottom-left square
-        (gap_width * 1.5, gap_height * 1.5),   # Bottom-right square
+        (gap_in_pixels / 2, gap_in_pixels / 2),           # Top-left square
+        (gap_in_pixels * 1.5, gap_in_pixels / 2),         # Top-middle square
+        (gap_in_pixels * 2.5, gap_in_pixels / 2),         # Top-right square
+        (gap_in_pixels / 2, gap_in_pixels * 1.5),         # Middle-left square
+        (gap_in_pixels * 1.5, gap_in_pixels * 1.5),       # Center square
+        (gap_in_pixels * 2.5, gap_in_pixels * 1.5),       # Middle-right square
+        (gap_in_pixels / 2, gap_in_pixels * 2.5),         # Bottom-left square
+        (gap_in_pixels * 1.5, gap_in_pixels * 2.5),       # Bottom-middle square
+        (gap_in_pixels * 2.5, gap_in_pixels * 2.5),       # Bottom-right square
     ]
 
     for center in centers:
         x, y = center
-        place_text(image, drawable, x, y, "This API facilitates cryptocurrency portfolio management and user interactions, while also offering cryptocurrency data retrieval, with special administrative functions for enhanced control.")
+        place_text(image, drawable, x, y, "Text that will be centered in the grid.")
 
+    # Display the image
     gimp.Display(image)
     gimp.displays_flush()
 
