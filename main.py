ea05f49ac134edc5161b2b08f198ec814a436421
Quentin Franklin <qfranklin906@gmail.com>
2024-02-02 20:25:34 -0500

    added unused code

--- main.py
+++ main.py
@@ -71,13 +71,6 @@ for index, input_string in enumerate(config.INPUT_QR_STRINGS):
     # Set the 3D cursor to the origin
     bpy.context.scene.cursor.location = (0.0, 0.0, 0.0)
 
-    # Select curves within the new collection
-    for obj in collection.objects:
-        if obj.type == 'CURVE':
-            obj.select_set(True)
-        else:
-            obj.select_set(False)
-
     # Set active object to be the first selected curve to allow 'join' operator
     bpy.context.view_layer.objects.active = next(obj for obj in collection.objects if obj.select_get())
 
