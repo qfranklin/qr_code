912fb31a56343f7635c7a171a44f2c0f9c5c5eef
Quentin Franklin <qfranklin906@gmail.com>
2024-02-04 15:03:48 -0500

    baseplate thickness can be set instead of the qr code thickness controling everything

--- main.py
+++ main.py
@@ -84,14 +84,14 @@ for index, input_string in enumerate(config.INPUT_QR_STRINGS):
     collection = image_obj.users_collection[0]
 
     merged_object = utils.center_object(utils.merge_objects(collection))
-    utils.solidify_object(not config.INVERT_QR_CODE)
+    utils.solidify_object(not config.INVERT_QR_CODE, config.QR_CODE_THICKNESS)
 
     # Add baseplate to qr code
     bpy.ops.mesh.primitive_plane_add(size=1, enter_editmode=False, location=(0, 0, 0))
     bpy.context.active_object.dimensions = (image_obj.dimensions.x + config.QUIET_ZONE, image_obj.dimensions.y + config.QUIET_ZONE, 1)
     image_obj.users_collection[0].objects.link(bpy.context.active_object)
     bpy.context.collection.objects.unlink(bpy.context.active_object)
-    utils.solidify_object(config.INVERT_QR_CODE)
+    utils.solidify_object(config.INVERT_QR_CODE, config.BASEPLATE_THICKNESS)
 
     merged_object = utils.center_object(utils.merge_objects(collection))
     bpy.context.view_layer.objects.active = merged_object
